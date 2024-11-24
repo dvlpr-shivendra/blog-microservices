8b4b81da701533dbe6830e1bbbf88df318b7f187
Shivendra TechSter <shivendratechster@gmail.com>
2024-11-24 15:09:54 +0530

    add command to start posts db container to dev.tmux

--- dev.tmux.sh
+++ dev.tmux.sh
@@ -1,40 +1,39 @@
 #!/usr/bin/env bash
 
 # Kill existing session if it exists
-tmux kill-session -t microservices 2>/dev/null
+tmux kill-session -t blog-services 2>/dev/null
 
 # Create new session
-tmux new-session -s microservices -n services -d
+tmux new-session -s blog-services -n services -d
 
-# Split the window into panes
-tmux split-window -h
+# Split the window into 3 panes vertically
 tmux split-window -v
-tmux select-pane -t 0
 tmux split-window -v
 
-# Setup consul in the first pane
+# Start all docker services in detached mode
 tmux select-pane -t 0
-tmux send-keys "docker compose up consul" C-m
-
+tmux send-keys "docker compose up -d consul" C-m
 # Wait for consul to start
 sleep 5
-
-# Setup posts service with Air in the second pane
-tmux select-pane -t 1
-tmux send-keys "cd posts" C-m
+tmux send-keys "cd posts && docker compose up -d" C-m
+# Wait for postgres to start
+sleep 5
 tmux send-keys "air" C-m
 
-# Setup gateway with Air in the third pane
-tmux select-pane -t 2
+# Setup gateway with Air in the second pane
+tmux select-pane -t 1
 tmux send-keys "cd gateway" C-m
 tmux send-keys "air" C-m
 
-# Reserve the fourth pane for commands/testing
-tmux select-pane -t 3
+# Setup third pane for commands/testing
+tmux select-pane -t 2
 tmux send-keys "echo 'Development environment ready. Use this pane for testing.'" C-m
 
 # Set the layout
-tmux select-layout tiled
+tmux select-layout even-vertical
+
+# Enable mouse support
+tmux set -g mouse on
 
 # Attach to the session
-tmux attach-session -t microservices
+tmux attach-session -t blog-services
