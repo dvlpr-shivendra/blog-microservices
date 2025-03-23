d1e5fd5ec725705e1e26f3a098de58f454e81a0f
Shivendra Kumar <shivendratechster@gmail.com>
2025-03-23 17:51:05 +0530

    Setup Prometheus + Grafana

--- likes/src/instrumentation.ts
+++ likes/src/instrumentation.ts
@@ -1,21 +1,24 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} from "@opentelemetry/sdk-metrics";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
 
+// Trace exporter for Jaeger
 const traceExporter = new OTLPTraceExporter({
-  url: "http://localhost:4318/v1/traces", // Match your Jaeger service
+  url: "http://localhost:4318/v1/traces",
+});
+
+// Create Prometheus exporter
+const prometheusExporter = new PrometheusExporter({
+  port: 9464,
+  host: "0.0.0.0", // Listen on all interfaces
 });
 
 const sdk = new NodeSDK({
   serviceName: "Likes",
   traceExporter,
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
+  metricReader: prometheusExporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
